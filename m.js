ccbb415403af80a6da5034838927846355dd7203
mohiniverma2403 <mverma24032005@gmail.com>
2025-10-16 12:45:50 +0530

    JS

---
+++ m.js
@@ -0,0 +1,69 @@
+/*for(i=1 ; i<10 ; i++)
+{
+    if(i<=5)
+    {
+        console.log(null)
+    }
+    else{
+        console.log(i)
+    }
+}
+for(let i=1; i<5; i++)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + "*"
+    }
+    console.log(str)
+}
+*/
+/*for(let i=1; i<5; i++)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + "*"
+    }
+    console.log(str)
+}
+for(let i=5; i>=1; i--)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + "*"
+    }
+    console.log(str)
+}
+*/
+
+/*for(let i=1; i<5; i++)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + "*"
+    }
+    console.log(str)
+}
+for(let i=3; i>=1; i--)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + "*"
+    }
+    console.log(str)
+}
+*/
+
+for(let i=1; i<5; i++)
+{
+    let str=""
+    for(let j=1; j<=(2*i); j++)
+    {
+        str = str + "*"        
+    }
+    console.log(str)
+}
---
+++ name.js
@@ -0,0 +1,64 @@
+ for(let i=0;i<5;i++)
+ {
+  console.log("Mohini")
+  }
+  for(let i=0;i<10;i++)
+  {
+    console.log("Anshul")
+  }
+   for(let i=0;i<15;i++)
+   {
+        console.log("Anjali")
+   }
+ 
+   for(let i=0; i<=7; i++)
+    {
+    let str=""
+    for(let j=0; j<=7; j++)
+    {
+         str+="*"
+    }
+    console.log(str)
+    }
+let i = 0
+do{
+    let j = 0
+    let str = ""
+    do{
+        str += "*"
+        j++
+    }while(j<=5)
+    console.log(str)
+    i++
+}while(i<=5)
+  
+for(let i=7; i>=1; i--)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + "*"
+    }
+    console.log(str)
+}
+
+for(let i=1; i<=5; i++)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + "*"
+    }
+    console.log(str)
+}
+
+for(let i=1; i<=5; i++)
+{
+    let str=""
+    for(let j=1; j<=i; j++)
+    {
+         str= str + `${j}`
+    }
+    console.log(str)
+}
+
